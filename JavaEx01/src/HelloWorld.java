27506c46fa6ecd9468e65eade743587bc71a6169
MYCOM <MYCOM@DESKTOP-17U8B8V>
2024-07-08 10:38:35 +0900

    최초 커밋

---
+++ JavaEx01/src/HelloWorld.java
@@ -0,0 +1,13 @@
+//헬로 월드를 출력한다.
+public class HelloWorld {
+	public static void main(String[] args) {
+		/*
+		System.out.println("Hello, World");
+		//주석은 코드 실행을 하지 않는다.
+		System.out.println("Hello, World");
+		System.out.println("Hello, World");
+		*/
+		
+		/* 여러줄 주석을 하려면 /* 사이에 내용을 넣는다.  */
+	}
+}
